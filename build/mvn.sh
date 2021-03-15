ef773e7319c95961d5d767c10d70e79838d20895
DavidArellano <icro_123@hotmail.com>
2021-03-15 01:06:58 -0600

    Update mvn.sh

--- build/mvn.sh
+++ build/mvn.sh
@@ -4,7 +4,7 @@ echo "****************"
 echo "* Building jar! "
 echo "****************"
 A=${PWD##*/}
-PROJ=/home/sheen/proyect/jenkins/jenkins_home/workspace/
+PROJ=/home/admin/jenkins/jenkins_home/workspace/
 ROUTE="$PROJ$A"
 docker run --rm -v /root/.m2:/root/.m2 -v $ROUTE:/app -w /app maven:3-alpine "$@"
 echo $ROUTE
