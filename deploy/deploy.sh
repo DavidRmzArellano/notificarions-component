2002a528abeac32a690a326b7efdbb901b84be54
Sheen <1b9c9927b146d478@gmail.com>
2021-06-18 19:55:42 -0500

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-docker run --rm -ti -d --network provider-red --ip 172.16.1.5 -p8087:8087 --name sending-notifications sending-notifications:latest
+docker run -ti -d --network provider-red --ip 172.16.1.5 -p8087:8087 --name sending-notifications sending-notifications:latest
